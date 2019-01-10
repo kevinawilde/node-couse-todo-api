c7b7da9eb4ba2955e66d72e1bf6857e267a9445d
Kevin Wilde <kevin.a.wilde@gmail.com>
2019-01-10 00:08:44 -0600

    Added GET route

--- server/server.js
+++ server/server.js
@@ -15,9 +15,17 @@ app.post('/todos', (req, res) => {
 		text: req.body.text
 	});
 
-	todo.save().then((doc) =>{
+	todo.save().then((doc) => {
 		res.send(doc);
 	}, (e) => {
+		res.status(400).send(e);
+	});
+});
+
+app.get('/todos', (req, res) => {
+	Todo.find().then((todos) => {
+		res.send({todos})
+	} , (e) => {
 		res.status(400).send(e);
 	});
 })
