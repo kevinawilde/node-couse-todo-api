11776905874fd6cc235aa8add1c030dfbce361ed
Kevin Wilde <kevin.a.wilde@gmail.com>
2019-01-15 22:48:06 -0600

    add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ require('./config/config');
 const _ = require('lodash');
 var express = require('express');
 var bodyParser = require('body-parser');
+const bcrypt = require('bcryptjs');
 
 const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
@@ -104,7 +105,7 @@ app.patch('/todos/:id', (req, res) => {
 	})
 
 })
-
+//POST /users
 app.post('/users', (req, res) => {
 	var body = _.pick(req.body, ['email', 'password']);
 	var user = new User(body);
@@ -124,12 +125,33 @@ app.post('/users', (req, res) => {
 app.get('/users/me', authenticate, (req, res) => {
 	res.send(req.user);
 });
+// POST /users/login {email, password}
+
+app.post('/users/login', (req, res) => {
+	var body = _.pick(req.body, ['email', 'password']);
+	
+	User.findByCredentials(body.email, body.password).then((user) => {
+		res.send((user) => {
+			return user.generateAuthToken().then((token) => {
+				res.header('x-auth', token).send(user);
+			})
+		}).catch((e) => {
+			res.status(400).send();
+		});
+	}).catch((e) => {
+		res.status(400).send();
+	})
+	
+	
+});
+
 
 app.listen(port, () => {
 	console.log(`Listening on Port ${port}`);
 });
 
-// GET /todos/id
+
+
 
 
 
