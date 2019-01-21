b7fed75877e0ed7b1d93148ee011f52da4b06ecd
Kevin Wilde <kevin.a.wilde@gmail.com>
2019-01-20 21:08:19 -0600

    Added DELETE to server test

--- server/server.js
+++ server/server.js
@@ -140,7 +140,7 @@ app.post('/users/login', (req, res) => {
 });
 
 app.delete('/users/me/token', authenticate, (req, res) => {
-	req.user.removeToken(req.token).then(()=> {
+	req.user.removeToken(req.token).then(() => {
 		res.status(200).send;
 	}, () => {
 		res.status(400).send();
